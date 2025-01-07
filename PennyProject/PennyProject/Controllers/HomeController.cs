ab3221f5dd07df7c594132353f3e0d37ffd8028e
SUNSYS\penny.wu <penny.wu@onelab.tw>
2025-01-08 00:35:29 +0800

    add home page and img

--- PennyProject/PennyProject/Controllers/HomeController.cs
+++ PennyProject/PennyProject/Controllers/HomeController.cs
@@ -1,22 +1,69 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PennyProject.DataBase.MovieDB;
+using PennyProject.Helper;
 using PennyProject.Models;
 using System.Diagnostics;
+using PennyProject.Models.Enum;
+using static PennyProject.Models.Enum.CountryOrderEnum;
 
 namespace PennyProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly PennyMovieDBContext _dbcontext;
+        public HomeController(ILogger<HomeController> logger, PennyMovieDBContext dbContext)
         {
             _logger = logger;
+            _dbcontext = dbContext;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                //var userId = HttpContext.Session.GetString("UserId");
 
+                // get movie info
+                var movies = await _dbcontext.MovieInfos
+                            .GroupBy(m => m.Country)
+                            .Select(g => new
+                            {
+                                Country = g.Key,
+                                Movies = g.OrderByDescending(m => m.ReleaseDateTime).ToList()
+                            })
+                            .ToListAsync();
+
+                //order
+                var moviesByCountry = movies
+                    .OrderBy(g => GetCountryOrder(g.Country))
+                    .ToDictionary(
+                        g => g.Country,
+                        g => g.Movies.ToList()
+                    );
+
+                return View(moviesByCountry);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                NlogHelper.WriteLog(Helper.LogLevel.Error, ex.StackTrace, ex);
+            }
+            return View();
+           
+        }
+        private int GetCountryOrder(string country)
+        {
+            return country?.ToLower() switch
+            {
+                "chiness" => (int)CountryOrder.Chiness,
+                "americas" => (int)CountryOrder.Americas,
+                "japan" => (int)CountryOrder.Japan,
+                "korea" => (int)CountryOrder.Korea,
+                "europe" => (int)CountryOrder.Europe,
+                _ => 999
+            };
         }
 
         public IActionResult Privacy()
---
+++ PennyProject/PennyProject/Models/Enum/CountryOrderEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace PennyProject.Models.Enum
+{
+    public class CountryOrderEnum
+    {
+        public enum CountryOrder
+        {
+            Chiness = 1,
+            Americas = 2,
+            Japan = 3,
+            Korea = 4,
+            Europe = 5
+        }
+    }
+}
